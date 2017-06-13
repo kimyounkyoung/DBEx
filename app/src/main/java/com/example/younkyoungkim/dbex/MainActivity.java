412d4076594d68cfa462f3bb3f71c596ca0be0ef
kimyounkyoung <passposs2@naver.com>
2017-06-13 20:33:30 +0900

    #3 EventHandling

--- app/src/main/java/com/example/younkyoungkim/dbex/MainActivity.java
+++ app/src/main/java/com/example/younkyoungkim/dbex/MainActivity.java
@@ -5,17 +5,44 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editName,editCount,editResultName, editResultCount;
+    Button butInit, butInsert,butSelect;
+    MyDBHelper myHelper;
+    SQLiteDatabase sqlDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName=(EditText)findViewById(R.id.edit_group_name);
+        editCount=(EditText)findViewById(R.id.edit_group_count);
+        editResultName=(EditText)findViewById(R.id.edit_result_name);
+        editResultCount=(EditText)findViewById(R.id.edit_result_count);
+        butInit=(Button)findViewById(R.id.but_init);
+        butInsert=(Button)findViewById(R.id.but_insert);
+        butSelect=(Button)findViewById(R.id.but_select);
+
+        //DB생성
+        myHelper=new MyDBHelper(this);
+        //기존의 테이블이 존재하면 삭제하고 테이블을 새로 생성한다.
+        butInit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                sqlDB=myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB,1,2);
+                sqlDB.close();
+            }
+        });   //익명 클래스
     }
 
     class MyDBHelper extends SQLiteOpenHelper{
-
+        // idolDB라는 이름의 데이터 베이스가 생성된다.
         public MyDBHelper(Context context) {
             super(context, "idolDB", null, 1);
         }
